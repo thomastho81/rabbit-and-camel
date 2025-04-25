cea4b3d45e413ffa9276ee65000f1b64967b3414
thomastho81 <thom.bp@hotmail.com>
2025-04-25 14:11:47 -0300

    criando exchange do tipo topic, produzindo e consumindo mensagem via camel

---
+++ src/main/java/thomastho/learnin/rabbit_camel_spring/camel/consumer/RotaTopicConsumer.java
@@ -0,0 +1,26 @@
+package thomastho.learnin.rabbit_camel_spring.camel.consumer;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Profile({"camel-topic"})
+@Component
+public class RotaTopicConsumer extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("spring-rabbitmq:topic_01?exchangeType=topic&routingKey=meu.nome.eh.*&queues=topic_queue01")
+                .routeId("CONSUMER_TOPIC_1_ROUTE_ID")
+                .process(exchange -> {
+                    var color = exchange.getIn().getBody(String.class);
+                    System.out.println("===== Recebido Topic Queue 01: " + color + " =====  ");
+                });
+
+        from("spring-rabbitmq:topic_01?exchangeType=topic&routingKey=meu.nome.#&queues=topic_queue02")
+                .routeId("CONSUMER_TOPIC_2_ROUTE_ID")
+                .process(exchange -> {
+                    var color = exchange.getIn().getBody(String.class);
+                    System.out.println("===== Recebido Topic Queue 02: " + color + " =====  ");
+                });
+    }
+}
---
+++ src/main/java/thomastho/learnin/rabbit_camel_spring/camel/producer/TopicProducer.java
@@ -0,0 +1,25 @@
+package thomastho.learnin.rabbit_camel_spring.camel.producer;
+
+import org.apache.camel.ProducerTemplate;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import thomastho.learnin.rabbit_camel_spring.ColorMap;
+
+@Profile({"camel-topic"})
+@Component
+public class TopicProducer {
+
+    private final ProducerTemplate producerTemplate;
+
+    public TopicProducer(ProducerTemplate producerTemplate) {
+        this.producerTemplate = producerTemplate;
+    }
+
+    @Scheduled(fixedDelay = 5000)
+    public void send() {
+        String color = ColorMap.getColor();
+
+        producerTemplate.sendBody("spring-rabbitmq:topic_01?exchangeType=topic&routingKey=meu.nome.eh.thomas", color);
+    }
+}
